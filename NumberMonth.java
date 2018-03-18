0741f174d74142cb90ba67a115fa4d351e3cd414
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-18 13:23:41 +0700

    number month edit

--- NumberMonth.java
+++ NumberMonth.java
@@ -4,10 +4,8 @@ public class NumberMonth {
     public static void main(String[] args) {
         int month;
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Mời bạn nhập tháng: ");
         month = scanner.nextInt();
-
         switch (month) {
             // Các tháng 1, 3, 5, 7, 8, 10 và 12 có 31 ngày.
             case 1:
@@ -19,7 +17,6 @@ public class NumberMonth {
             case 12:
                 System.out.println("Tháng " + month + " có 31 ngày.");
                 break;
-
             // Các tháng 4, 6, 9 và 11 có 30 ngày
             case 4:
             case 6:
@@ -27,7 +24,6 @@ public class NumberMonth {
             case 11:
                 System.out.println("Tháng " + month + " có 30 ngày.");
                 break;
-
             //  Tháng 2 nếu là năm nhuận thì có 29 ngày, còn không thì có 28 ngày.
             case 2:
                 int year;
